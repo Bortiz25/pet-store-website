3fb988bf1f884ea9dec9e414b5466a3959e66eb1
lcarter056 <94881226+lcarter056@users.noreply.github.com>
2024-02-28 12:35:41 -0600

    removed second App.listen statement

--- pet_website_app/app.js
+++ pet_website_app/app.js
@@ -50,8 +50,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}`);
-});
-
 module.exports = app;
