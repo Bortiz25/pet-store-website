d4bbf7e7fa6a06ebe07cca5d07135d5beb97552d
lcarter056 <94881226+lcarter056@users.noreply.github.com>
2024-02-29 00:45:15 -0600

    product class and fetching

---
+++ pet_website_app/models/product.js
@@ -0,0 +1,36 @@
+
+
+const mongoose = require('mongoose');
+
+// Schema with fields from products collection in MongoDB
+const productSchema = new mongoose.Schema({
+productName: String,
+price: Number,
+tags: Array,
+category: String,
+img: String,
+description: String
+});
+
+//product class starter code
+class ProductClass {
+    constructor(productName, price, tags, category, img, description) {
+    this.productName = productName;
+    this.price = price;
+    this.tags = tags;
+    this.category = category;
+    this.img = img;
+    this.description = description;
+    }
+   
+    // methods
+    display() {}
+    getName() {}
+    getDescription() {}
+    getPrice(){}
+}    
+
+// product collection from mongoDB
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = { ProductClass, Product };
