1cf40c3e05be1c7e34838c5333b8c41d84a809ab
Gayatri Rajamony <gayatrirajamony@gmail.com>
2024-03-07 18:32:03 -0600

    finished basic about page, added fonts

--- pet_website_app/routes/about.js
+++ pet_website_app/routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/about', { title: 'About' });
+  res.render('pages/about', { title: 'About Us' });
 });
 
 module.exports = router;
