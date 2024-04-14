0fc3dbeb8a7c5c7c29ede9be0c2986f136a562cd
lcarter056 <94881226+lcarter056@users.noreply.github.com>
2024-04-14 16:20:30 -0500

    productController complete

--- pet_website_app/routes/adminPage.js
+++ pet_website_app/routes/adminPage.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const productFunctions = require ('../controllers/productController');
-const {Product, ProductClass} = require ('../models/product');
 
     //route to delete product 
     router.post('/:productId', async (req,res,next)=> {
     try { 
-     await Product.deleteOne({productName: req.params.productId});
+     await productFunctions.deleteProduct(req.params.productId);
      await productFunctions.addAudit(req.params.productId, "Seth", "Deleted product");
     res.redirect('/pages/adminPage');
     } catch (err){
-        console.log("error deleting product", error);
+        console.log("Error deleting product", error);
     }
     });
 
