ae25f7fc46f6fdc142fc20a4ac5f6e879bf61c14
lcarter056 <94881226+lcarter056@users.noreply.github.com>
2024-04-19 11:15:00 -0500

    adminSession

--- pet_website_app/routes/login.js
+++ pet_website_app/routes/login.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const userFunctions = require ('../controllers/userController');
-
 const {User, UserClass} = require ('../models/user');
 
 
 router.post('/', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-     // userInfo is in JSON format. The pw value is a boolean 
-     const {userInfo, pw} = await userFunctions.findUser(username, password);
-     //if pw is true, user entered correct pw
-     if(pw){    
+     // userInfo is in JSON and the 'access' value is a boolean 
+     const {userInfo, access} = await userFunctions.findUser(username, password);
+     // if 'access' is true, user entered correct username and pw 
+     if(access){    
         //adding users to the session
         await User.findOne({userName:username, password: password}).then((user) => {
             req.session.user = {
                 username: user.userName,
+                name: user.firstName,
                 isAdmin: user.isAdmin,
             }
         });
+
         if(userInfo.isAdmin){
             res.redirect('adminPage');
         } else {
